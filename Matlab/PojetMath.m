8e6dec34476e60b47effc5e4ab5797a352adcb38
adele <adele.marcon@protonmail.com>
2025-11-20 11:30:21 +0100

    variation lambda

--- Matlab/PojetMath.m
+++ Matlab/PojetMath.m
@@ -20,7 +20,7 @@ w21 = 1;
 w22 = 0;
 W = [w11, w12 ; w21, w22];
 B = [0.5; 0.5];
-s0 = [0 ; 10];
+s0 = [5 ; 10];
 
 Phi2 = @(x) 1 ./ (1 + exp(-b*x));
 
@@ -40,37 +40,11 @@ for i = 1:N
     xlabel('Temps (t)', 'FontSize', 12);
     ylabel('États s_i(t)', 'FontSize', 12);
     grid on;
-    legend(['s_' num2str(i) '(t)'], 'Location', 'best', 'FontSize', 11);
+    legend(['s_1' num2str(i) '(t)'],['s_2' num2str(i) '(t)'], 'Location', 'best', 'FontSize', 11);
     hold on
 end
 
 hold off
-
-%% 
-% Graphique 1
-subplot(1, 3, 1);
-plot(t1, s1, 'LineWidth', 2);
-xlabel('Temps (t)', 'FontSize', 12);
-ylabel('États s_i(t)', 'FontSize', 12);
-grid on;
-legend('s_1(t)', 's_2(t)', 'Location', 'best', 'FontSize', 11);
-
-% Graphique 2
-subplot(1, 3, 2);
-plot(t2, s2, 'LineWidth', 2);
-xlabel('Temps (t)', 'FontSize', 12);
-ylabel('États s_i(t)', 'FontSize', 12);
-grid on;
-legend('s_1(t)', 's_2(t)', 'Location', 'best', 'FontSize', 11);
-
-
-% Graphique 3
-subplot(1, 3, 3);
-plot(t3, s3, 'LineWidth', 2);
-xlabel('Temps (t)', 'FontSize', 12);
-ylabel('États s_i(t)', 'FontSize', 12);
-grid on;
-legend('s_1(t)', 's_2(t)', 'Location', 'best', 'FontSize', 11);
 
 %% Fonction positive
 clear all;
@@ -78,10 +52,7 @@ close all;
 clc;
 
 n = 2;
-lambda1 = 0.001
-lambda2 = 1
-lambda3 = 10
-b = 1
+b = 1;
 w11 = 0;
 w12 = 1;
 w21 = 1;
@@ -94,8 +65,8 @@ Phi1 =  @(x) max(0,x);
 ed1 = @(t1, s1) -lambda1 * s1 + Phi1(W * s1 + B);
 ed2 = @(t2, s2) -lambda2 * s2 + Phi1(W * s2 + B);
 ed3 = @(t3, s3) -lambda3 * s3 + Phi1(W * s3 + B);
-s0 = [0 ; 10]
-tspan = [0 2]
+s0 = [0 ; 10];
+tspan = [0 2];
 
 [t1, s1] = ode45(ed1, tspan, s0);
 [t2, s2] = ode45(ed2, tspan, s0);
